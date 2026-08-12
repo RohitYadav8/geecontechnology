24d4b4c9cef3513125a9a8a3bcd65e07f6f0f7ce
RohitYadav8 <rohityadav40823@gmail.com>
2026-08-12 13:55:44 +0530

    modified prisma/lib.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
-  connectionLimit: 5,
+  connectionLimit: 3,
 });
 
 export const prisma =
