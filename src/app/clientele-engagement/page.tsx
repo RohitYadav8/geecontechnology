5519d93e9842b9ca6b553d4d7bcaa06366911efc
RohitYadav8 <rohityadav40823@gmail.com>
2026-08-14 18:04:43 +0530

    updated page clientele-engagement

--- src/app/clientele-engagement/page.tsx
+++ src/app/clientele-engagement/page.tsx
@@ -66,7 +66,7 @@ export default function ClienteleEngagementPage() {
         {/* Background Effects */}
         <BackgroundEffects />
 
-        {/* Hero Section */}
+        
         <ClientHero />
 
         {loading ? (
